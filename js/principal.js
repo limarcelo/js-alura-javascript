e2b6896c8edee76b9376fd344e0a65f7839ee713
Marcelo Lima <limarcelo90@gmail.com>
2017-10-03 17:36:21 -0400

    calculando e validando imc

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,42 @@
 
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista - MS";
+
+var pesoValido = true;
+var alturaValida = true;
+
+var paciente =  document.querySelector("#primeiro-paciente")
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+console.log(paciente);
+
+//imprimindo os valores
+console.log(peso);
+console.log(altura);
+//---
+
+var tdImc = paciente.querySelector(".info-imc");
+
+
+if(peso < 0 || peso > 1000){
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if(altura < 0 || altura > 3.00){
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if(pesoValido && alturaValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc ;
+}
+
+
+
