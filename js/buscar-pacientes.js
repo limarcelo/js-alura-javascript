578c4e1135017ea044b16a6c6fe75fa7067ba4bf
Marcelo Lima <limarcelo90@gmail.com>
2017-10-05 16:57:27 -0400

    commit final

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -3,15 +3,24 @@ var botaoBuscar = document.querySelector("#buscar-pacientes");
 
 botaoBuscar.addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
+    var erroAjax = document.querySelector("#erro-ajax");
 
     xhr.open("GET", "http://localhost:3000/data");
 
     xhr.addEventListener("load", function () {
-        var pacientes = JSON.parse(xhr.responseText);
 
-        pacientes.forEach(function (paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+        if(xhr.status == 200){
+            var pacientes = JSON.parse(xhr.responseText);
+
+            erroAjax.classList.add("invisivel");
+
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        }else{
+
+            erroAjax.classList.remove("invisivel");
+        }
     });
 
     xhr.send();
